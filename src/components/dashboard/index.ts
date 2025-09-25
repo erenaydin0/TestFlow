3679f59ea248b1fb93bb11df3f014a34fff01977
erenaydin0 <erenaydin.esa@gmail.com>
2025-09-25 17:15:23 +0300

    StatsCards bileşeni kaldırıldı, diğer bileşenler korundu.

--- src/components/dashboard/index.ts
+++ src/components/dashboard/index.ts
@@ -1,4 +1,3 @@
-export { default as StatsCards } from './StatsCards';
 export { default as DailyTestResults } from './DailyTestResults';
 export { default as TestSuiteDistribution } from './TestSuiteDistribution';
 export { default as SuccessRateTrend } from './SuccessRateTrend';
