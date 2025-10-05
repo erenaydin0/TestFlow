98c1a9988bab8ab5dfd1ef9b56adfb68468015b6
erenaydin0 <erenaydin.esa@gmail.com>
2025-10-05 17:56:35 +0300

    Geliştirilmiş tarih filtreleme işlevi için ortak bileşenler dizinine DateRangeFilter dışa aktarımı eklendi.

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,6 @@
 // Common components
 export { default as DataFilters } from './DataFilters';
+export { default as DateRangeFilter } from './DateRangeFilter';
 export { default as DataTable } from './DataTable';
 export { default as LoadingErrorState } from './LoadingErrorState';
 export { default as MultiSelect } from './MultiSelect';
