520fa02e25c5b067f14e9214bc593367d7184cd4
erenaydin0 <erenaydin.esa@gmail.com>
2025-10-10 03:27:22 +0300

    Remove CosmicSpinner component and integrate its functionality into LoadingErrorState. Update exports in common index file for improved organization and maintainability.

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,10 +2,9 @@
 export { default as DataFilters } from './DataFilters';
 export { default as DateRangeFilter } from './DateRangeFilter';
 export { default as DataTable } from './DataTable';
-export { default as LoadingErrorState } from './LoadingErrorState';
+export { default as LoadingErrorState, CosmicSpinner } from './LoadingErrorState';
 export { default as MultiSelect } from './MultiSelect';
 export { default as CustomSelect } from './CustomSelect';
 export { default as StatusBadge, getStatusColor, getStatusText } from './StatusBadge';
 export { default as CosmicLogo } from './CosmicLogo';
-export { default as CosmicSpinner } from './CosmicSpinner';
 export { EditableSuiteCell, EditableTagsCell, EditableBrowserCell } from './EditableCells';
